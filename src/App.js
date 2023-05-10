97df06d2cb61ee71dba3daf3cdd91282dad4aa85
E-moL0587 <kai.pika.025@outlook.com>
2023-05-10 13:46:30 +0900

    First Commit

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          EmoL <code>src/App.js</code> and save to reload.
+          蹊モンガのwebサイトを作成中です
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://twitter.com/seikei_Pokecir"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Twitter
         </a>
       </header>
     </div>
