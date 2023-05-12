7933e939b46bf3c8a42be4dc1a0b49e434fc6d79
E-moL0587 <kai.pika.025@outlook.com>
2023-05-12 16:52:52 +0900

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
@@ -13,12 +14,28 @@ function App() {
           (※公式webサイトを作成中です)
         </p>
 
-        <a
-          className="App-link"
-          href="https://twitter.com/seikei_Pokecir"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+
+        <div className="Event">
+        
+        <div class="d-demo">
+          <div class="d-demo__wrap">
+            <ul class="d-demo__list d-demo__list--left">
+                  <li class="d-demo__item"><img src="https://contents.oricon.co.jp/upimg/news/20100415/75336_201004150404257001271301978c.jpg" alt="mypika"/></li>
+                  <li class="d-demo__item"><img src="https://contents.oricon.co.jp/upimg/news/20100415/75336_201004150429496001271301978c.jpg" alt="mypika"/></li>
+                  <li class="d-demo__item"><img src="https://contents.oricon.co.jp/upimg/news/20100415/75336_201004150438102001271301978c.jpg" alt="mypika"/></li>
+            </ul>
+            <ul class="d-demo__list d-demo__list--left">
+                  <li class="d-demo__item"><img src="https://contents.oricon.co.jp/upimg/news/20100415/75336_201004150404257001271301978c.jpg" alt="mypika"/></li>
+                  <li class="d-demo__item"><img src="https://contents.oricon.co.jp/upimg/news/20100415/75336_201004150429496001271301978c.jpg" alt="mypika"/></li>
+                  <li class="d-demo__item"><img src="https://contents.oricon.co.jp/upimg/news/20100415/75336_201004150438102001271301978c.jpg" alt="mypika"/></li>
+            </ul>
+
+
+         </div>
+       </div>
+       </div>
+
+        <a className="App-link" href="https://twitter.com/seikei_Pokecir" target="_blank" rel="noopener noreferrer">
           ポケモンサークル公式Twitter
         </a>
 
@@ -27,10 +44,15 @@ function App() {
          成蹊大学ポケモンサークル 蹊モンガ は、<br></br>
          ポケモンが大好きな人が集まるサークルです！<br></br>
         </p>
+        <br></br>
         <img src={"mypika.jpg"} alt="gphoto1" width="200" />
          <br></br>
         <img src={"emol.jpg"} alt="gphoto2" width="200" />
          <br></br>
+        <img src={"https://aniyuki.com/wp-content/uploads/2022/03/aniyuki-pokemon-67.gif"} alt="gphoto2" width="200" />
+         <br></br>
+
+
 
       </header>
     </div>
