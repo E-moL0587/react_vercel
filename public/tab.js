7af4b8371dfec95a7e55aac1ae3a6ec03a8dbebc
E-moL0587 <kai.pika.025@outlook.com>
2023-05-14 17:40:44 +0900

    First Commit

--- public/tab.js
+++ public/tab.js
@@ -1,11 +1,27 @@
-$(".openbtn1").click(function () {//ボタンがクリックされたら
-  $(this).toggleClass('active');//ボタン自身に activeクラスを付与し
-    $("#g-nav").toggleClass('panelactive');//ナビゲーションにpanelactiveクラスを付与
-    $(".circle-bg").toggleClass('circleactive');//丸背景にcircleactiveクラスを付与
-});
-
-$("#g-nav a").click(function () {//ナビゲーションのリンクがクリックされたら
-    $(".openbtn1").removeClass('active');//ボタンの activeクラスを除去し
-    $("#g-nav").removeClass('panelactive');//ナビゲーションのpanelactiveクラスを除去
-    $(".circle-bg").removeClass('circleactive');//丸背景のcircleactiveクラスを除去
+// タブ
+const tabList = document.querySelectorAll('.tab-list-item');
+// タブコンテンツ
+const tabContent = document.querySelectorAll('.tab-contents-item');
+ 
+//DOMが読み込み終わったら
+document.addEventListener('DOMContentLoaded', function(){
+  // タブに対してクリックイベントを適用
+  for(let i = 0; i < tabList.length; i++) {
+    tabList[i].addEventListener('click', tabSwitch);
+  }
+  // タブをクリックすると実行する関数
+  function tabSwitch(){
+    // activeクラスを削除
+    document.querySelectorAll('.active')[0].classList.remove('active');
+    // クリックしたタブにactiveクラスを付与    
+    this.classList.add('active');
+    // showクラスを削除
+    document.querySelectorAll('.show')[0].classList.remove('show');
+    // タブを配列風オブジェクトとして定義
+    const aryTabs = Array.prototype.slice.call(tabList);
+    // クリックしたタブの配列番号を取得     
+    const index = aryTabs.indexOf(this);
+    // クリックしたタブと同じ配列番号のタブコンテンツにshowクラスを付与    
+    tabContent[index].classList.add('show');
+  };
 });
