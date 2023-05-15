7bc6f40456d554c3dc692dfbd2a3270c510aaa60
E-moL0587 <kai.pika.025@outlook.com>
2023-05-15 10:30:24 +0900

    first commit

--- public/damage.js
+++ public/damage.js
@@ -1,22 +1,21 @@
-let sv;
-let iv;
-let lv;
-
-//関数作成
 function func() {
 
-    sv = document.getElementById('sv').value; //種族値の値を取得
-    iv = document.getElementById('iv').value; //個体値の値を取得
-    lv = document.getElementById('lv').value; //レベルの値を取得
+    let sv = Number(document.getElementById('sv').value); //種族値の値を取得
+    let ev = Number(document.getElementById('ev').value); //努力値の値を取得
+    let mv = Number(document.getElementById('mv').value); //技威力の値を取得
+    let iv = Number(document.getElementById('iv').value); //個体値の値を取得
+    let lv = Number(document.getElementById('lv').value); //レベルの値を取得
+    let mm = Number(document.getElementById('mm').value); //補正値の値を取得
 
     //入力した値が0以上の場合計算処理する
     if (parseFloat(sv) > 0) {  //parseFloat(文字列変数)⇒数値に変換
 
-        //BMI計算
-        let damage = sv;
+        let A = Math.floor((Math.floor((sv*2+iv+Math.floor(ev/4))*lv/100)+5)*mm);
+        let B = 189
+        let damage = Math.floor(Math.floor(Math.floor(lv*2/5+2)*mv*A/B)/50+2);
 
-        //計算式表示
-        document.getElementById("damage_value").value = Math.round(damage * 100) / 100;  //小数点桁数指定で四捨五入の場合、桁数をずらしてあげる.    
+        document.getElementById("damage_value0.85").value = Math.floor(damage*0.85);
+        document.getElementById("damage_value1.00").value = Math.floor(damage*1.00);
     }
 
     // エラー表示
