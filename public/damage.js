bb59c3d9c03aa31ffc129bdefc80d6d70b80c3ca
E-moL0587 <kai.pika.025@outlook.com>
2023-05-15 09:32:21 +0900

    first commit

--- public/damage.js
+++ public/damage.js
@@ -1,31 +1,24 @@
-let weight; //変数 体重
-let height; //変数 身長
+let sv;
+let iv;
+let lv;
 
 //関数作成
 function func() {
 
-    weight = document.getElementById('weight').value; //体重の値を取得
-    height = document.getElementById('height').value; //身長の値を取得
+    sv = document.getElementById('sv').value; //種族値の値を取得
+    iv = document.getElementById('iv').value; //個体値の値を取得
+    lv = document.getElementById('lv').value; //レベルの値を取得
 
     //入力した値が0以上の場合計算処理する
-    if (parseFloat(weight) > 0 && parseFloat(height) > 0) {  //parseFloat(文字列変数)⇒数値に変換
+    if (parseFloat(sv) > 0) {  //parseFloat(文字列変数)⇒数値に変換
 
         //BMI計算
-        let bmi = weight / (height * height);
+        let damage = sv;
 
         //計算式表示
-        document.getElementById("bmi_weight_value").value = weight;
-        document.getElementById("bmi_height_value1").value = height;
-        document.getElementById("bmi_height_value2").value = height;
-
-        //BMI結果表示
-        document.getElementById("bmi_value").value = Math.round(bmi * 100) / 100;  //小数点桁数指定で四捨五入の場合、桁数をずらしてあげる.    
-    }
-    //入力された値が0以上でない場合エラー
-    else {
-        //エラー表示
-        alert("値をちゃんと入れてくれないと悲しみます(´；ω；`)ｳｩｩ...");
+        document.getElementById("damage_value").value = Math.round(damage * 100) / 100;  //小数点桁数指定で四捨五入の場合、桁数をずらしてあげる.    
     }
 
-
+    // エラー表示
+    else { alert("値をちゃんと入れてくれないと悲しみます(´；ω；`)ｳｩｩ..."); }
 }
