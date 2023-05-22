bfc44d77e66e6e18500f9598559ff7998543fd21
E-moL0587 <kai.pika.025@outlook.com>
2023-05-22 23:36:21 +0900

    First Commit

--- public/quiz.js
+++ public/quiz.js
@@ -1,3 +1,4 @@
+
 const pokemonImage = document.getElementById('pokemon-image');
 const guessInput = document.getElementById('guess-input');
 const submitButton = document.getElementById('submit-button');
@@ -5,7 +6,8 @@ const result = document.getElementById('result');
 
 // ランダムなポケモンのIDを生成する関数
 const getRandomPokemonId = () => {
-  return Math.floor(Math.random() * 1000) + 1;
+  // return Math.floor(Math.random() * 1009) + 1;
+  return Math.floor(Math.random() * 151) + 1;
 };
 
 // ポケモンの問題を作成する関数
@@ -33,7 +35,7 @@ const createPokemonQuestion = () => {
         if (userGuess === pokemonName) {
           result.textContent = `正解です！このポケモンは ${userGuess} です。`;
         } else {
-          result.textContent = `不正解です... このポケモンは ${pokemonName} です。あなたの答えは ${userGuess} です。`;
+          result.textContent = `不正解です... このポケモンは「 ${pokemonName} 」です。あなたの答えは「 ${userGuess} 」です。`;
         }
 
         // 答えを提出ボタンから削除
@@ -44,7 +46,7 @@ const createPokemonQuestion = () => {
         setTimeout(() => {
           result.textContent = '';
           createPokemonQuestion();
-        }, 2000);
+        }, 5000);
       };
 
       // Enterキーで答えを提出する関数
