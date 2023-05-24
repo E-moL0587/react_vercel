73595106dfa62a18b25b052130104d1bc2ea16ba
E-moL0587 <kai.pika.025@outlook.com>
2023-05-24 17:41:35 +0900

    First Commit

--- public/countdown2.js
+++ public/countdown2.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
     var countdownElement = document.getElementById("countdown2");
-    var count = 10; // カウントダウンの秒数
+    var count = 60; // カウントダウンの秒数
   
     function updateCountdown() {
       countdownElement.innerHTML = count + "秒";
