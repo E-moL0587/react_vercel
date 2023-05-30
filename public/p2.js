4aad943a15602a84558e8c398d2e77f9a0ba4be2
E-moL0587 <kai.pika.025@outlook.com>
2023-05-31 08:52:08 +0900

    First Commit

--- public/p2.js
+++ public/p2.js
@@ -3,8 +3,8 @@ const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
 // キャンバスのサイズをウィンドウサイズに合わせる
-canvas.width = window.innerWidth * 0.9;
-canvas.height = window.innerHeight * 0.9;
+canvas.width = window.innerWidth * 0.95;
+canvas.height = window.innerHeight * 0.8;
 
 // パーティクルの設定
 const particleCount = 6;  // パーティクルの数
@@ -16,7 +16,7 @@ let mouseY = 0;
 
 // パーティクルオブジェクトの定義
 class Particle {
-  constructor(x, y, size, spriteURL) {
+  constructor(x, y, size, spriteURL, shinySpriteURL) {
     this.x = x;
     this.y = y;
     this.size = size;
@@ -24,17 +24,46 @@ class Particle {
     this.baseY = this.y;
     this.density = Math.random() * 30 + 1;  // パーティクルの密度
     this.spriteURL = spriteURL;
+    this.isShiny = Math.random() < 0.05;  // 5%の確率で色違いになる
     this.sprite = new Image();
-    this.sprite.src = spriteURL;
+    this.sprite.src = this.isShiny ? shinySpriteURL : spriteURL;
+  }
+
+  calculateDistance(otherParticle) {
+    const dx = otherParticle.x - this.x;
+    const dy = otherParticle.y - this.y;
+    return Math.sqrt(dx ** 2 + dy ** 2);
   }
 
   update() {
+    particles.forEach(otherParticle => {
+      if (otherParticle !== this) {
+        const distance = this.calculateDistance(otherParticle);
+        const minDistance = this.size + otherParticle.size;  // パーティクル同士の最小距離
+
+        if (distance < minDistance) {
+          const dx = otherParticle.x - this.x;
+          const dy = otherParticle.y - this.y;
+          const angle = Math.atan2(dy, dx);
+          const targetX = this.x + Math.cos(angle) * minDistance;
+          const targetY = this.y + Math.sin(angle) * minDistance;
+          const offsetX = (targetX - otherParticle.x) * 0.1;  // 調整係数
+          const offsetY = (targetY - otherParticle.y) * 0.1;  // 調整係数
+
+          this.x -= offsetX;
+          this.y -= offsetY;
+          otherParticle.x += offsetX;
+          otherParticle.y += offsetY;
+        }
+      }
+    });
+
     const dx = mouseX - this.x;
     const dy = mouseY - this.y;
     const distance = Math.sqrt(dx ** 2 + dy ** 2);
     const forceDirectionX = dx / distance;
     const forceDirectionY = dy / distance;
-    const maxDistance = 100;
+    const maxDistance = 30;
     const force = (maxDistance - distance) / maxDistance;
     const directionX = forceDirectionX * force * this.density;
     const directionY = forceDirectionY * force * this.density;
@@ -68,8 +97,9 @@ async function init() {
 
     const pokemonData = await getRandomPokemon();
     const spriteURL = pokemonData.sprites.front_default;
+    const shinySpriteURL = pokemonData.sprites.front_shiny;
 
-    const particle = new Particle(x, y, size, spriteURL);
+    const particle = new Particle(x, y, size, spriteURL, shinySpriteURL);
     particles.push(particle);
   }
 }
@@ -115,4 +145,3 @@ window.addEventListener('mousemove', updateMousePosition);
 window.addEventListener('touchmove', (e) => {
   updateMousePosition(e.touches[0]);
 });
-
