91cca1866b9aef9d53676514957c2cfc575c002e
E-moL0587 <kai.pika.025@outlook.com>
2023-06-02 12:02:57 +0900

    First Commit

--- public/load.js
+++ public/load.js
@@ -1,8 +1,4 @@
 document.addEventListener("DOMContentLoaded", function() {
   var loadingScreen = document.getElementById("loading-screen");
-  
-  // ページの読み込みが完了したらローディング画面を非表示にする
-  window.addEventListener("load", function() {
-    loadingScreen.style.display = "none";
-  });
+  loadingScreen.style.display = "none";
 });
